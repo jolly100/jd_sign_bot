fe4bc9458265a6b92e2fd0807527b2e80c3aa394
jolly100 <39402891+jolly100@users.noreply.github.com>
2020-08-21 09:00:21 +0800

    Update app.js

--- app.js
+++ app.js
@@ -58,8 +58,9 @@ async function start() {
     if (fs.existsSync(path)) {
       content = fs.readFileSync(path, "utf8");
     }
-    await sendNotify("京东签到-" + new Date().toLocaleDateString(), content);
-    console.log('发送结果完毕')
+//    await sendNotify("京东签到-" + new Date().toLocaleDateString(), content);
+      await sendNotify("京东签到-" + new Date().toLocaleDateString() + "-" + (KEY.substring(KEY.lastIndexOf("pt_pin")+7, KEY.lastIndexOf(";"))), content);
+      console.log('发送结果完毕')
   }
 }
 
